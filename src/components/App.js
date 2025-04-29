3d6d11ff5c4c1b6cf9719af276012f282ffa4c32
Kelvin Ndunda <kelvinndunda.github@gmail.com>
2025-04-29 09:39:48 +0300

    created basic react components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import About from "./About";
 function NavBar() {
   return (
     <nav>
@@ -23,8 +23,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <About/>
     </div>
   );
 }
